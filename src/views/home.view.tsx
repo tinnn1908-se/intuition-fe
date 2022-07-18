531fdd94471861bc1b8650d0e7d815b434ef31b9
tinnn1908-se <tinnn1908.se@gmail.com>
2022-07-18 13:04:22 -0500

    categories

--- src/views/home.view.tsx
+++ src/views/home.view.tsx
@@ -4,6 +4,7 @@ import '../styles/home.style.scss'
 import { IProduct } from '../models/product.model';
 import ProductService from '../services/product.service';
 import Banner from '../components/banner.component';
+import Categories from '../components/categories.component';
 
 
 const HomeView = () => {
@@ -23,8 +24,9 @@ const HomeView = () => {
     <div className='home'>
       <Header />
       <Banner />
-      {/* <Banner/>
       <Categories/>
+      {/*
+      
       <Products title={APPLICATION.PRODUCT_TITLE_PRODUCTS} products={products}/>
       <Trend/>
       <Discount/>
