439c1f17641358f6ad1d0cf0b4415c8af6fc6ef5
tinnn1908-se <tinnn1908.se@gmail.com>
2022-07-18 12:51:11 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,25 @@ import { Routes, Route } from 'react-router-dom'
 import HomeView from './views/home.view';
 function App() {
   return (
-    <div className="App">
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: 'fitContent',
-        backgroundColor: '#F6F7FB',
-        margin: '0',
-        padding: '0',
-        boxSizing: 'border-box'
-      }} className='app'>
-        <Routes>
-          {/* <Route path='/register' element={<RegisterView/>}  />
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: 'fitContent',
+      backgroundColor: '#F6F7FB',
+      margin: '0',
+      padding: '0',
+      width: '100%',
+      boxSizing: 'border-box'
+    }} className='app'>
+      <Routes>
+        {/* <Route path='/register' element={<RegisterView/>}  />
             <Route path='/login' element={<LoginView/>}  />
             <Route path='/' element={<HomeView/>} />
             <Route path='/ProductDetail/:productID' element={<ProductDetailView/>}/>
             <Route path='/checkout' element={<CheckoutView/>} ></Route>
             <Route path='/productview' element={<ProductsView/>} ></Route> */}
-          <Route path='/' element={<HomeView />} />
-        </Routes>
-      </div>
+        <Route path='/' element={<HomeView />} />
+      </Routes>
     </div>
   );
 }
