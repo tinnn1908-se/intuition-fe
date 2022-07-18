8ce838c3c6e45edd85b9fb111d3cb0b65b854dcb
tinnn1908-se <tinnn1908.se@gmail.com>
2022-07-17 22:25:14 -0500

    changes

---
+++ src/views/home.view.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import Header from '../components/header.component'
+import '../styles/home.style.scss'
+import { IProduct } from '../models/product.model';
+import ProductService from '../services/product.service';
+
+
+const HomeView = () => {
+
+  const [products, setProducts] = useState<Array<IProduct>>([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      var response = await ProductService.getNewestProducts();
+      setProducts(response);
+    }
+    fetchProducts();
+  }, [])
+
+
+  return (
+    <div className='home'>
+      <Header/>
+      {/* <Banner/>
+      <Categories/>
+      <Products title={APPLICATION.PRODUCT_TITLE_PRODUCTS} products={products}/>
+      <Trend/>
+      <Discount/>
+      <Footer/> */}
+    </div>
+  )
+}
+
+export default HomeView
