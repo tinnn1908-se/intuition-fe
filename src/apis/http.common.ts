565c08a8e905523addb2ee6b6452a3e24d71b75d
tinnn1908-se <tinnn1908.se@gmail.com>
2022-07-28 00:54:02 -0500

    fix

--- src/apis/http.common.ts
+++ src/apis/http.common.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL : "https://intuition-be.herokuapp.com"
-    baseURL : "http://localhost:8080"
+    baseURL : "https://intuition-be.herokuapp.com"
+    // baseURL : "http://localhost:8080"
 })
